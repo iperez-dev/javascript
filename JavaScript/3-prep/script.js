c294eebff4f69357d132d6f49adf32afa687bb65
Ivan Perez <ivan.web9450@gmail.com>
2023-08-01 21:39:28 -0400

    commit

--- JavaScript/3-prep/script.js
+++ JavaScript/3-prep/script.js
@@ -19,4 +19,45 @@
 //return `Hello ${name}`
 //return arrPowersOfTwo.push(Math.pow(2, i));
 //return n % 2 === 0;
-//.split(" ").map((item) => `${item.substr(1)}${item[0]}ay`).join(" ");
+//return .split(" ").map((item) => `${item.substr(1)}${item[0]}ay`).join(" ");
+//return return str === str.toUpperCase()
+//return Math.floor(Math.random(x) * 10)
+
+/////////////////////////////////////////////////////////////
+// Question29 | ky8 | Parse nice int from char problem
+
+/*
+You ask a small girl,"How old are you?" She always says, "x years old", where x is a random number between 0 and 9.
+
+Write a program that returns the girl's age (0-9) as an integer.
+
+Assume the test input string is always a valid string. For example, the test input may be "1 year old" or "5 years old". The first character in the string is always a number.
+*/
+
+//input -> function girlAge(x) ->
+//return -> "x year old" (random num 0 <= x <= 9)
+//possible sol -> random number between 0 and 9? | if else
+
+//example
+function girlAge(x) {
+  let randomNum = Math.floor(Math.random(x) * 10);
+  if (randomNum > 1) {
+    return `${randomNum} years old`;
+  } else {
+    return `${randomNum} year old`;
+  }
+}
+
+//test cases
+console.log(girlAge(0));
+
+/////////////////////////////////////////////////////////////
+// Question30 | ky8 | Twice as old
+
+/*
+Your function takes two arguments:
+
+current father's age (years)
+current age of his son (years)
+Сalculate how many years ago the father was twice as old as his son (or in how many years he will be twice as old). The answer is always greater or equal to 0, no matter if it was in the past or it is in the future.
+*/
