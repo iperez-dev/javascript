d410334034f8f3fbda1fd9b7c60d61d445de07e2
Ivan Perez <ivan.web9450@gmail.com>
2023-08-11 07:36:35 -0400

    3-color-changer

---
+++ JavaScript/3-color-changer/script.js
@@ -0,0 +1,10 @@
+document.getElementById("btn").addEventListener("click", changeColor);
+
+let index = 0;
+
+function changeColor() {
+  let colors = ["green", "red", "blue", "green", "orange", "purple"];
+
+  document.getElementsByTagName("body")[0].style.background = colors[index++];
+  if (index > colors.length - 1) index = 0;
+}
