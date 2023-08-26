57eb31fde9aad911bf20738cb80d9e1b2dfd7719
Ivan Perez <ivan.web9450@gmail.com>
2023-08-26 11:10:52 -0400

    api

--- JavaScript/3-apis/script.js
+++ JavaScript/3-apis/script.js
@@ -1,12 +1,12 @@
 //API -> Application Programming Interface
 //JSON -> JavaScript Object Notation
+// document.querySelector("img").src = data.message;
 // DOG.CEO API
 
-fetch("https://dog.ceo/api/breed/husky/images/random")
+fetch(`https://dog.ceo/api/breed/boxer/images/random`)
   .then((res) => res.json()) //parse response as JSON
   .then((data) => {
     console.log(data);
-    document.querySelector("img").src = data.message;
   })
 
   .catch((err) => {
